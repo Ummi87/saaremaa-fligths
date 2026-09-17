26fff2fbdc673142451d963b5701d58cb68f49bc
Ummi87 <96148610+Ummi87@users.noreply.github.com>
2026-09-17 19:19:36 +0300

    feat: lisa config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+/* Muuda siin Saaremaa jälgimisala ja ajajoone esituskiirust. */
+window.FLIGHT_CONFIG = {
+  bbox: { lamin: 57.85, lomin: 21.5, lamax: 58.75, lomax: 23.6 },
+  snapshotIntervalMinutes: 5,
+  playbackIntervalMs: 700,
+  dataUrl: "data/flights_24h.json",
+  mapAttribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+};
